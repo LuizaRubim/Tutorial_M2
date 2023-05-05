ba472516219a489ce24fb81d77fc354b6bb6204c
LuizaRubim <luiza.rubim@sou.inteli.edu.br>
2023-05-04 21:53:42 -0300

    entrega autoestudo semana 3

---
+++ SEMANA_03/02_AUT_EST_ENTREGA/backend/app_41.js
@@ -0,0 +1,105 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = 'data/curriculo.db';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+/* Colocar toda a parte estática no frontend */
+app.use(express.static("frontend/"));
+
+/* Definição dos endpoints */
+/******** CRUD ************/
+app.use(express.json());
+
+// Retorna todos registros (é o R do CRUD - Read)
+app.get('/curriculos', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM dados_pessoais ORDER BY nome COLLATE NOCASE';
+		db.all(sql, [],  (err, rows ) => {
+			if (err) {
+				throw err;
+			}
+			res.json(rows);
+		});
+		db.close(); // Fecha o banco
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/insereCurriculo', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	sql = "INSERT INTO dados_pessoais (nome, cargo, endereço, telefone, email, descricao_trajetoria) VALUES ('" + req.body.nome + "', '" + req.body.cargo + "', '" + req.body.endereco + "','" + req.body.telefone + "','" + req.body.email + "','" + req.body.descricao + "');";
+
+
+	console.log(sql);
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}	
+	});
+	res.write('<p>CURRICULO INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Monta o formulário para o update (é o U do CRUD - Update)
+app.get('/pegarDados', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "SELECT * FROM dados_pessoais WHERE cod_pessoa="+ req.query.cod_pessoa;
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+	db.close(); // Fecha o banco
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.post('/atualizar', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "UPDATE dados_pessoais SET nome='" + req.body.nome + "', cargo = '" + req.body.cargo + "', endereço = '" + req.body.endereco + "' , telefone='" + req.body.telefone + "', email = '" + req.body.email + "', descricao_trajetoria = '" + req.body.descricao + "' WHERE cod_pessoa='" + req.body.cod_pessoa + "'";
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	res.write('<p>CURRICULO ATUALIZADO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.get('/removeCurriculo', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	sql = "DELETE FROM dados_pessoais WHERE cod_pessoa='" + req.query.cod_pessoa + "'";
+	console.log(sql);
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.write('<p>CURRICULO REMOVIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
---
+++ SEMANA_03/02_AUT_EST_ENTREGA/curriculo.sql
@@ -0,0 +1,64 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "experiencia" (
+	"cod_experiencia"	INTEGER NOT NULL,
+	"cod_pessoa"	INTEGER,
+	"nome_da_empresa"	TEXT NOT NULL,
+	"ano_inicio"	INTEGER NOT NULL,
+	"ano_fim"	INTEGER,
+	"cargo_experiencia"	TEXT,
+	"descricao_experiencia"	INTEGER,
+	FOREIGN KEY("cod_pessoa") REFERENCES "dados_pessoais"("cod_pessoa"),
+	PRIMARY KEY("cod_experiencia" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "dados_pessoais" (
+	"cod_pessoa"	INTEGER NOT NULL,
+	"nome"	TEXT NOT NULL,
+	"cargo"	TEXT,
+	"endereço"	TEXT,
+	"telefone"	NUMERIC,
+	"email"	TEXT,
+	"descricao_trajetoria"	TEXT,
+	PRIMARY KEY("cod_pessoa" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "personalidade" (
+	"cod_personalidade"	INTEGER NOT NULL,
+	"cod_pessoa"	INTEGER NOT NULL,
+	"nome_personalidade"	TEXT,
+	"nivel_personalidade"	INTEGER,
+	FOREIGN KEY("cod_pessoa") REFERENCES "dados_pessoais"("cod_pessoa"),
+	PRIMARY KEY("cod_personalidade" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "habilidade" (
+	"cod_hab"	INTEGER NOT NULL,
+	"cod_pessoa"	INTEGER NOT NULL,
+	"nome_habilidade"	INTEGER,
+	"nivel_habilidade"	INTEGER,
+	FOREIGN KEY("cod_pessoa") REFERENCES "dados_pessoais"("cod_pessoa"),
+	PRIMARY KEY("cod_hab" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "formacao" (
+	"cod_curso"	INTEGER NOT NULL,
+	"cod_pessoa"	INTEGER,
+	"nome_curso"	TEXT NOT NULL,
+	"ano_inicio"	INTEGER NOT NULL,
+	"ano_fim"	INTEGER,
+	"descricao_curso"	TEXT,
+	FOREIGN KEY("cod_pessoa") REFERENCES "dados_pessoais"("cod_pessoa"),
+	PRIMARY KEY("cod_curso" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "realizacao" (
+	"cod_realizacao"	INTEGER NOT NULL,
+	"cod_pessoa"	INTEGER NOT NULL,
+	"nome_realizacao"	TEXT NOT NULL,
+	"ano_realizacao"	INTEGER NOT NULL,
+	"descricao_realizacao"	TEXT,
+	FOREIGN KEY("cod_pessoa") REFERENCES "dados_pessoais"("cod_pessoa"),
+	PRIMARY KEY("cod_realizacao" AUTOINCREMENT)
+);
+INSERT INTO "dados_pessoais" ("cod_pessoa","nome","cargo","endereço","telefone","email","descricao_trajetoria") VALUES (1,'Luiza Souza Rubim','estudante','Rua M.M.D.C,80 - Butantã, São Paulo','(27)996218352','luiza.rubim@sou.inteli.edu.br','aaaaaaaaaaaaaaaaaaaaaaa');
+INSERT INTO "habilidade" ("cod_hab","cod_pessoa","nome_habilidade","nivel_habilidade") VALUES (1,1,0,0);
+INSERT INTO "formacao" ("cod_curso","cod_pessoa","nome_curso","ano_inicio","ano_fim","descricao_curso") VALUES (1,1,'Centro Educacional Leonardo da Vinci',2018,2022,'ensino fundamental e médio com bolsa integral
+'),
+ (2,1,'Inteli',2023,'-','Graduação em Engenharia da Computação');
+INSERT INTO "realizacao" ("cod_realizacao","cod_pessoa","nome_realizacao","ano_realizacao","descricao_realizacao") VALUES (1,1,'',0,NULL);
+COMMIT;
